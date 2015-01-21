8a9e7f412ac7e961bd29764cc4f190c103ade9e8
Maxwell Barvian <max@barvian.me>
2015-01-21 09:27:55 -0600

    Install vim-sensible

--- Scripts/install.sh
+++ Scripts/install.sh
@@ -11,6 +11,7 @@ mkdir -p ~/.vim/autoload ~/.vim/bundle && \
 curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
 
 echo "Installing vim plugins"
+git clone git://github.com/tpope/vim-sensible.git ~/.vim/bundle/sensible
 git clone git://github.com/altercation/vim-colors-solarized.git ~/.vim/bundle/solarized
 
 echo "Hiding files…"
