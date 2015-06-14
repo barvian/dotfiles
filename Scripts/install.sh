3023ec791efa831712559108d2119bad053ba99d
Maxwell Barvian <max@barvian.me>
2015-06-14 11:48:13 -0500

    Update install.sh

--- Scripts/install.sh
+++ Scripts/install.sh
@@ -2,9 +2,8 @@ echo "Changing shell…"
 chsh -s /bin/zsh
 
 echo "Linking Sublime Text configuration to Dropbox"
-cd ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/
-rm -r User
-ln -s ~/Dropbox/Development/Sublime\ Text/User
+rm -r ~/.atom
+ln -s ~/Dropbox/Apps/Atom ~/.atom
 
 echo "Installing pathogen.vim"
 mkdir -p ~/.vim/autoload ~/.vim/bundle && \
@@ -33,8 +32,8 @@ rbenv global 2.1.3
 
 echo "Installing Node…"
 git clone https://github.com/creationix/nvm.git ~/.nvm && cd ~/.nvm && git checkout `git describe --abbrev=0 --tags`
-nvm install 0.10.32
-nvm alias default 0.10.32
+nvm install 0.12.4
+nvm alias default 0.12.4
 npm install -g npm
 
 echo "Done."
