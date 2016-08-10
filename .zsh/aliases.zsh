ebbf3a71c7fa4e9bb27361e28094050db4425430
Maxwell Barvian <max@barvian.me>
2016-08-10 10:13:58 -0500

    Update aliases.zsh

--- .zsh/aliases.zsh
+++ .zsh/aliases.zsh
@@ -13,13 +13,6 @@ alias gs='git status'
 compdef _git gs=git-status
 alias gut='git'
 
-# Rails
-alias tlog='tail -f log/development.log'
-alias b='bundle exec'
-alias bake='bundle exec rake'
-alias rr='rbenv rehash'
-alias bm='bin/m'
-
 # Mac OS X
 alias o='open .'
 alias screensaver='/System/Library/Frameworks/ScreenSaver.framework/Versions/A/Resources/ScreenSaverEngine.app/Contents/MacOS/ScreenSaverEngine'
